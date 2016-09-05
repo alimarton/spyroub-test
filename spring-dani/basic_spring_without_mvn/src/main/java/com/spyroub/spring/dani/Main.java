b6feefe21820f4cd4ff1a0da8ddba80c744b0cb2
szdani33 <daniel.tamas.szabo@gmail.com>
2016-09-05 11:13:51 +0200

    gitignore improvements and lib filder

--- spring-dani/basic_spring_without_mvn/src/main/java/com/spyroub/spring/dani/Main.java
+++ spring-dani/basic_spring_without_mvn/src/main/java/com/spyroub/spring/dani/Main.java
@@ -7,7 +7,6 @@ import com.spyroub.spring.dani.writer.ConsoleWriter;
 
 public class Main {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		ApplicationContext ac = new GenericXmlApplicationContext("spring-beans.xml");
 		System.out.println(ac.getBeansOfType(ConsoleWriter.class));
