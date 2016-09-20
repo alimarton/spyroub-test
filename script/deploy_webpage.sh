63b087adef9757c1679e7bc47dc0a065dfeeb547
szdani33 <daniel.tamas.szabo@gmail.com>
2016-09-20 17:14:42 +0200

    automatic deploy try 4

--- script/deploy_webpage.sh
+++ script/deploy_webpage.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
 # copy the newest version of the index.html to the remote server
-scp -P 80 website/index.html $USER_NAME:$PASSWORD@$HOST:$TARGET_FOLDER
+scp -P 22 website/index.html $USER_NAME:$PASSWORD@$HOST:$TARGET_FOLDER
